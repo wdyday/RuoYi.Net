94d719ab642c924b96a8fcce911470b943e4e9fe
wdyday <wdyday@163.com>
2023-12-13 09:35:14 +0800

    fix

--- RuoYi.Framework/Interceptors/TransactionalInterceptor.cs
+++ RuoYi.Framework/Interceptors/TransactionalInterceptor.cs
@@ -5,6 +5,12 @@ using SqlSugar;
 
 namespace RuoYi.Framework.Interceptors
 {
+    /// <summary>
+    /// 在要被拦截的方法上标注 [Transactional], 
+    /// 类需要是public类，方法如果需要拦截就是[虚方法]，
+    /// 支持异步方法，因为动态代理是动态生成被代理的类的动态子类实现的。
+    /// </summary>
+
     public class TransactionalAttribute : AbstractInterceptorAttribute
     {
         [FromServiceContext]
@@ -24,6 +30,8 @@ namespace RuoYi.Framework.Interceptors
             {
                 Log.Error("Transactional Error", ex);
                 await Db.Ado.RollbackTranAsync();
+
+                throw ex;
             }
         }
     }
